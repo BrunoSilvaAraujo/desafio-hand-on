8d6759d0908b6edf2d7b149020c6869b39577848
BrunoSilvaAraujo <teste1nodejs.com>
2021-01-08 14:15:04 -0300

    incluindo package.json

--- app.js
+++ app.js
@@ -5,24 +5,20 @@ const app = express()
 const port = 3000
 
 
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
+  let response = `<html><head><title>Desafio</title></head><body>`;
 
-	let response = `<html><head><title>Desafio</title></head><body>`
+  await axios.get(process.env.URL).then((resp) => {
+    resp.data.map((item, i) => {
+      if (i <= 99) {
+        response += `<h3>${item.albumId} / ${item.id} - ${item.title}</h3><img src='${item.url}'/><br />`;
+      }
+    });
+  });
 
-	await axios.get(process.env.URL ).then(resp => {
-
-		resp.data.map((item, i) => {
-		if ( i <= 99 ){
-				response += `<h3>${item.albumId} / ${item.id} - ${item.title}</h3><img src='${item.url}'/><br />`
-			}
-		})
-	})
-	
-
-	response += `</body>`
-
-	res.send(response)
-})
+  response += `</body>`;
+  res.send(response);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
